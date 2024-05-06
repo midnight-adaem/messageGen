2c4107242d1f1efe4268da6611ad6daa923212b8
midnight-adaem <163080516+midnight-adaem@users.noreply.github.com>
2024-05-06 11:37:43 -0400

    works now!

--- messageGen.js
+++ messageGen.js
@@ -1,8 +1,15 @@
 // setup data sources
+const nouns = ['fish','couch','tree','plane','goat','car','bike','civil engineer','mercenary','goose','hammer','planet','Imperial Majestrix','snowball','rare earth element'];
+const verbs = ['fish','run','sleep','dance','build','decay','grow','fester','ferment','mutate','shimmy','boogie','photosynthesize','nerf','work'];
+const adverbs = ['fishily','slowly','quickly','nicely','infuriatingly','fungally','sinisterly','majestically','weirdly','radioactively','chaotically','randomly','stinkily','freshly','gnomically'];
 
 // grab 3 random data elements
+let noun = nouns[Math.floor(Math.random() * nouns.length)];
+let verb = verbs[Math.floor(Math.random() * verbs.length)];
+let adverb = adverbs[Math.floor(Math.random() * adverbs.length)];
+
 
 // output message
-let message = "Hi!";
+let message = `Hi! My ${noun} likes to ${verb} ${adverb}!`;
 
 console.log (message);
